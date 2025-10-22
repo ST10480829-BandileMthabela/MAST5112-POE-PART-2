882544535652b6ec5f4656f99b944243dc976479
ST10480829-BandileMthabela <ST10480829@vcconnect.edu.za>
2025-10-22 18:03:11 +0200

    imported Image and TouchableOpacity, added an image and a button for navigation (not working yet), installed navigatioon dependencies, styled the image so it is visable on expo go (for testing).

--- SplashScreen.tsx
+++ SplashScreen.tsx
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function SplashScreen() {
+
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+// 5 components are used in for this screen. 
+// styleSheet: makes the app look good. 
+// text: to give the user information on different elements in the app.
+// view: allows the user to see the coded components.
+// images: help the user identify and get an alternitive view of what is trying to be expressed by the app. 
+// TouchableOpacity: The button will be used to navigate to the home screen.
+
+import {NavigationContainer} from '@react-navigation/native';
+//installed 'react-navigation/native' dependency to remove an error.
+
+
+
+
+
+
+
+export default function SplashScreen({navigation}:any)
+//Changed default function to Navigation.
+{
   return (
     <View style={styles.container}>
-      <Text>Open up SplashScreen.tsx to start working on your app!</Text>
+      <Text>WELCOME TO CAFE FRESH</Text>
+    
+      <Image 
+        source={{ uri: 'https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687' }}
+        style={styles.image}
+      />
+
+       <TouchableOpacity
+       onPress={()=> navigation.navigate('')}
+       ></TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +45,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 300,
+    height: 350,
+    borderRadius: 10,
+    marginVertical: 20,
   },
 });
